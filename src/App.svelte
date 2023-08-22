9bea9786f6c82780753fa49acb9314233c15c6a7
minokuma <ksky216@naver.com>
2023-08-22 21:09:40 +0900

    1 file modified

--- src/App.svelte
+++ src/App.svelte
@@ -100,12 +100,15 @@
       autoplayProgressVisible={false}
       arrows={false}
       swiping={true}
-      
     >
     {#each images as src, imageIndex (src)}
       <div class="container">
         {#if loaded.includes(imageIndex)}
-          <img src={src.url} alt={src.description} class=img-container  />
+          <img 
+            src={src.url} 
+            alt={src.description} 
+            class=img-container  
+          />
           <div>{text}</div>
         {/if}
       </div>
@@ -114,7 +117,20 @@
     <div class="overlay-appbar-right">
       <div class="flex-none lg:hidden">
         <label for="my-drawer-3" class="btn btn-square btn-ghost">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="none" 
+            viewBox="0 0 24 24" 
+            class="inline-block w-6 h-6 stroke-current"
+          >
+            <path 
+              stroke-linecap="round" 
+              stroke-linejoin="round" 
+              stroke-width="2" 
+              d="M4 6h16M4 12h16M4 18h16"
+            >
+            </path>
+          </svg>
         </label>
       </div>
     </div>

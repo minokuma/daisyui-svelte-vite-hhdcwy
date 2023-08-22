414f48d202370221df9dba58fb6925a76c61a36f
minokuma <ksky216@naver.com>
2023-08-22 13:14:51 +0900

    1 file modified

--- src/App.svelte
+++ src/App.svelte
@@ -3,14 +3,14 @@
   <div class="drawer-content flex flex-col">
     <!-- Navbar -->
     <nav class="sticky top-0">
+    
       <div class="w-full navbar bg-base-300">
-        
         <div class="flex-1 px-2 mx-2 table-pin-rows">꾸민</div>
         <div class="flex-none hidden lg:block">
           <ul class="menu menu-horizontal">
             <!-- Navbar menu content here -->
             <li><a>홈</a></li>
-            <li><a>로그인 </a></li>
+            <li><a>로그인</a></li>
           </ul>
         </div>
         <div class="flex-none lg:hidden">
@@ -21,15 +21,12 @@
       </div>
     </nav>
     <!-- Page content here -->
-    내용<br/>내용<br/>내용<br/>내용<br/>내용<br/>
-    내용<br/>내용<br/>내용<br/>내용<br/>내용<br/>
-    내용<br/>내용<br/>내용<br/>내용<br/>내용<br/>
-    내용<br/>내용<br/>내용<br/>내용<br/>내용<br/>
-    내용<br/>내용<br/>내용<br/>내용<br/>내용<br/>
-    내용<br/>내용<br/>내용<br/>내용<br/>내용<br/>
-    내용<br/>내용<br/>내용<br/>내용<br/>내용<br/>
-    내용<br/>내용<br/>내용<br/>내용<br/>내용<br/>
-    내용<br/>내용<br/>내용<br/>내용<br/>내용<br/>
+    <br/><br/><br/><br/><br/>
+    <br/><br/><br/><br/><br/>
+    <br/><br/><br/><br/><br/>
+    <br/><br/><br/><br/><br/>
+    <br/><br/><br/><br/><br/>
+    <br/><br/><br/><br/><br/>
     내용<br/>내용<br/>내용<br/>내용<br/>내용<br/>
     내용<br/>내용<br/>내용<br/>내용<br/>내용<br/>
     내용<br/>내용<br/>내용<br/>내용<br/>내용<br/>
@@ -62,7 +59,8 @@
           </div>
           <div class="flex-none lg:hidden">
             <label for="my-drawer-3" class="btn btn-square btn-ghost">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+              <!-- svg 속성 -->
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
             </label>
           </div>
         </div>

8619ab92ebda9ff5375e0be5deb47bd595709d18
minokuma <ksky216@naver.com>
2023-08-22 12:54:19 +0900

    1 file modified

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,22 @@
-<div class="drawer">
+<div class="drawer drawer-end">
   <input id="my-drawer-3" type="checkbox" class="drawer-toggle" /> 
   <div class="drawer-content flex flex-col">
     <!-- Navbar -->
     <nav class="sticky top-0">
       <div class="w-full navbar bg-base-300">
-        <div class="flex-none lg:hidden">
-          <label for="my-drawer-3" class="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-          </label>
-        </div> 
-        <div class="flex-1 px-2 mx-2 table-pin-rows">네비바 타이틀</div>
+        
+        <div class="flex-1 px-2 mx-2 table-pin-rows">꾸민</div>
         <div class="flex-none hidden lg:block">
           <ul class="menu menu-horizontal">
             <!-- Navbar menu content here -->
             <li><a>홈</a></li>
             <li><a>로그인</a></li>
           </ul>
+        </div>
+        <div class="flex-none lg:hidden">
+          <label for="my-drawer-3" class="btn btn-square btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+          </label>
         </div>
       </div>
     </nav>
@@ -47,12 +48,28 @@
   </div> 
   <div class="drawer-side">
     <label for="my-drawer-3" class="drawer-overlay"></label> 
-    <ul class="menu p-4 w-80 h-full bg-base-100 ">
+    <ul class="menu w-full h-full bg-gray-100 text-gray-300">
+      <!-- Navbar -->
+      <nav class="sticky top-0">
+        <div class="w-full navbar text-gray-600">
+          <div class="flex-1 px-2 mx-2 table-pin-rows">꾸민</div>
+          <div class="flex-none hidden lg:block">
+            <ul class="menu menu-horizontal">
+              <!-- Navbar menu content here -->
+              <li><a>홈</a></li>
+              <li><a>로그인</a></li>
+            </ul>
+          </div>
+          <div class="flex-none lg:hidden">
+            <label for="my-drawer-3" class="btn btn-square btn-ghost">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+            </label>
+          </div>
+        </div>
+      </nav>
       <!-- Sidebar content here -->
       <li><a>홈</a></li>
       <li><a>항목</a></li>
-      
     </ul>
-    
   </div>
 </div>

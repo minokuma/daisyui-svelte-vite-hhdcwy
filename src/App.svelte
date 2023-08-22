29a25a7c41c68f2baec146fb02a5d163b5641f31
minokuma <ksky216@naver.com>
2023-08-22 17:45:55 +0900

    3 files modified

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,38 @@
+<script>
+  import Carousel from "svelte-carousel";
+  const images = [
+    {
+      url: "https://res.cloudinary.com/beswift/image/upload/v1650391490/photo-1644241687200-eadaf7601290_xcz2kh.jpg",
+      description: "image1",
+    },
+    {
+      url: "https://res.cloudinary.com/beswift/image/upload/v1650391337/photo-1647067867267-e01d98462f3c_ugtnwe.jpg",
+      description: "image2",
+    },
+    {
+      url: "https://res.cloudinary.com/beswift/image/upload/v1650391131/photo-1648800475313-2bb7fbec8701_ae60yw.jpg",
+      description: "image3",
+    },
+    {
+      url: "https://res.cloudinary.com/beswift/image/upload/v1650390102/photo-1649894222226-056a1a79d9fb_xlv73h.jpg",
+      description: "image4",
+    },
+    {
+      url: "https://res.cloudinary.com/beswift/image/upload/v1650389425/photo-1649894221695-45abb718a190_sgjhwd.jpg",
+      description: "image5",
+    },
+  ];
+  let carousel; // for calling methods of the carousel instance
+  const handleNextClick = () => {
+    carousel.goToNext();
+  };
+</script>
+
 <div class="drawer drawer-end">
   <input id="my-drawer-3" type="checkbox" class="drawer-toggle" /> 
+  
   <div class="drawer-content flex flex-col">
-    <!-- Navbar -->
+    <!-- Navbar start -->
     <nav class="sticky top-0">
       <div class="w-full navbar bg-base-300">
         <div class="flex-1 px-2 mx-2 table-pin-rows">꾸민</div>
@@ -19,6 +50,22 @@
         </div>
       </div>
     </nav>
+    <!-- Navbar  -->
+    <Carousel
+      bind:this={carousel}
+      let:loaded
+      autoplay
+      autoplayDuration={3000}
+      autoplayProgressVisible
+    >
+      {#each images as src, imageIndex (src)}
+        <div class="img-container">
+          {#if loaded.includes(imageIndex)}
+            <img src={src.url} alt={src.description}  />
+          {/if}
+        </div>
+      {/each}
+  </Carousel>
     <!-- Page content here -->
     <br/><br/><br/><br/><br/>
     <br/><br/><br/><br/><br/>

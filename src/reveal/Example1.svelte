9384496bc75c03294deb7d3ac5389025a5c42149
minokuma <ksky216@naver.com>
2023-08-26 07:50:04 +0900

    1 file modified

--- src/reveal/Example1.svelte
+++ src/reveal/Example1.svelte
@@ -6,13 +6,16 @@
 <Label>Example 1</Label>
 <h1 use:reveal={{ transition: "fade", delay: 50 }}>꾸민과 함께 꾸며!</h1>
 <h2 use:reveal={{ delay: 100 }}>상상의 나래를 펼쳐~</h2>
+
 <div class="box">
 	<h3 use:reveal={{ delay: 600}}>이 글을 보게될 거에요~</h3>
 	<h3 use:reveal={{ delay: 1200}}>이어서 이 글까지요~</h3>
-  <h3 use:reveal={{ transition: "blur", delay: 1300}}>이어서 이 글까지요~</h3>
-  <h3 use:reveal={{ transition: "scale", delay: 1400}}>이어서 이 글까지요2~</h3>
-  <h3 use:reveal={{ transition: "fly", delay: 1400}}>이어서 이 글까지요2~</h3>
 </div>
+
+<h3 use:reveal={{ transition: "blur", delay: 1300}}>이어서 이 글까지요~</h3>
+<h3 use:reveal={{ transition: "scale", delay: 1400}}>이어서 이 글까지요2~</h3>
+<h3 use:reveal={{ transition: "fly", delay: 1400}}>이어서 이 글까지요2~</h3>
+
 <p use:reveal={{ delay: 1800 }}>그렇죠? 😎</p>
 <p use:reveal={{ delay: 2000 }}>
   <img src="https://img1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/guest/image/5rdoC4dVtpbnZRtlgnbSUJH59rM.png" />

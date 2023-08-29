530aa2aaced56735aab56132de2c3979e32a09c4
minokuma <ksky216@naver.com>
2023-08-29 20:18:52 +0900

    1 file modified, 1 added, 1 deleted

--- src/reveal/Example1.svelte
+++ src/reveal/Example1.svelte
@@ -236,6 +236,56 @@
   </div>
 </div>
 
+
+
+
+
+<div class="box h-screen w-screen w-full bg-gradient-to-r from-indigo-500 from-20% via-40% via-sky-500 via-60% to-emerald-500 to-80% flex flex-col justify-center items-center">
+  <div class="w-full md:w-[55%] md:flex md:justify-center">
+    {#if isMobile}
+      <div use:reveal={{ x: -50, y: 10 }} class="md:w-full mb-4">
+        <h3 use:reveal={{ transition: "blur", delay: 0 }} class="text-center text-white">주문제작 디자인
+			</div>
+			<p use:reveal={{ transition: "blur", delay: 300 }} 
+				class="text-xl text-center text-gray-200 text-1xl font-fh font-bold leading-normal mb-1">내가 원하는 디자인을
+			</p>
+			<p use:reveal={{ transition: "blur", delay: 500 }} 
+				class="text-xl text-center text-gray-200 text-1xl font-fh font-bold leading-normal mb-1">꾸민 앱에 담아
+			</p>
+			<p use:reveal={{ transition: "blur", delay: 700 }} 
+				class="text-xl text-center text-gray-200 text-5xl font-fh font-bold leading-normal mt-1">쉽게 공유할 수 있어요.
+			</p>
+      <div use:reveal={{ x: -10, y: 150 }} class="md:w-full transform rotate-90 transform-origin-center">
+        <img class="max-w-full h-auto max-w-[70%]" src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FEWpzF%2FbtqGfMB0vZl%2FkqkwUwBXr2XyCQjNICA4x1%2Fimg.png"  />
+      </div>
+			
+    {:else}
+			<div use:reveal={{ x: -50, y: 10 }} class="md:w-full mb-4" >
+        <h1
+					use:reveal={{ transition: "blur", delay: 100 }} 
+					class="text-left text-white text-5xl font-fh font-bold leading-relaxed">주문제작 디자인
+				</h1>
+				<h3
+					use:reveal={{ transition: "blur", delay: 300 }} 
+					class="text-left text-white text-5xl font-fh font-bold leading-relaxed">내가 원하는 디자인을
+				</h3>
+				<h3
+					use:reveal={{ transition: "blur", delay: 500 }} 
+					class="text-left text-white text-5xl font-fh font-bold leading-relaxed">꾸민 앱에 담아
+				</h3>
+				<h3
+					use:reveal={{ transition: "blur", delay: 700 }} 
+					class="text-left text-white text-5xl font-fh font-bold leading-relaxed">쉽게 공유할 수 있어요.
+				</h3>
+    	</div>
+      <div use:reveal={{ x: -10, y: 150 }} class="md:max-w-[50%] md:mr-2 mx-20 flex transform rotate-90 transform-origin-center">
+        <img class="max-w-full h-auto max-w-[100%] md:w-[100%]" src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FEWpzF%2FbtqGfMB0vZl%2FkqkwUwBXr2XyCQjNICA4x1%2Fimg.png" />
+      </div>
+    {/if}
+  </div>
+</div>
+
+
 <!-- 카드 -->
 <div class="h-screen w-screen w-full bg-gradient-to-r from-emerald-400 from-10% via-sky-300 via-30% to-emerald-500 to-90% flex flex-col justify-center items-center">
 	<div class="card w-96 glass">
@@ -265,3 +315,4 @@
     </div>
 </div>
  -->
+
